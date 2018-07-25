22f6ca7acd445da995f235ec2a69fa0cf2e29d8e
Jessy Hiroquoy <hiroquoy.jessy@gmail.com>
2018-07-24 22:32:03 -0400

    router setup

--- src/components/HomeCard/HomeCard.jsx
+++ src/components/HomeCard/HomeCard.jsx
@@ -9,6 +9,7 @@ import { Button } from '@material-ui/core';
 import green from '@material-ui/core/colors/green';
 import { withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
+import { Link } from 'react-router-dom'
 
 import './HomeCard.css'
 
@@ -30,49 +31,70 @@ class HomeCard extends Component {
             value: 250000,
         };
         this.setSliderValue = this.setSliderValue.bind(this);
+        this.sendData = this.sendData.bind(this);
     }
     
     setSliderValue = (event, value) => {
         this.setState({ value });
     };
 
+    sendData() {
+        const { dataAggregate } = this.props;
+
+        dataAggregate(this.state);
+    }
+
     render() {
         const { value } = this.state;
 
         return (
-            <Grid className="card-container" container>
-                <Grid item lg={6} md={6} sm={6} xs={12}>
-                    <Card raised>
-                        <CardContent>
-                            <Typography variant='display1' style={{ color: '#2A5CAD' }}>
-                                Quick, Easy and 100% Secure
+            <div>
+                <div>
+                    <h1 className="main-text">
+                        America's #1 Business Funding Company!
+                    </h1>
+                </div>
+                <Grid className="card-container" container>
+                    <Grid item lg={5} md={6} sm={6} xs={12}>
+                        <Card raised>
+                            <CardContent>
+                                <Typography variant='display1' style={{ color: '#2A5CAD' }}>
+                                    Quick, Easy and 100% Secure
 
-                            </Typography>
-                        </CardContent>
-                        <CardContent>
-                            <Typography variant='subheading' style={{ color: '#0e3e5a', padding: 0 }}>
-                                One Simple Application, and No Financial Statements up to $500K!
+                                </Typography>
+                            </CardContent>
+                            <CardContent>
+                                <Typography variant='subheading' style={{ color: '#0e3e5a', padding: 0 }}>
+                                    One Simple Application, and No Financial Statements from $5000 up to $500K!
 
-                            </Typography>
-                        </CardContent>
-                        <CardContent>
-                            <SimpleSlider setSliderValue={this.setSliderValue} value={value}/>
-                        </CardContent>
-                        <CardContent>
-                            <Button onClick={() => console.log(value)}
-                                variant='contained' size='large' style={{ color: 'white' }} className={classNames(this.props.classes.cssRoot)}>
-                                Apply Now
-                                <i className="material-icons">
-                                    arrow_forward
-                                </i>
-                            </Button>
-                        </CardContent>
-                        <CardContent>
-                            It's <b>free</b> & won't affect your credit score
-                        </CardContent>
-                    </Card>
-                </ Grid>
-            </Grid>
+                                </Typography>
+                            </CardContent>
+                            <CardContent>
+                                <SimpleSlider setSliderValue={this.setSliderValue} value={value}/>
+                            </CardContent>
+                            <CardContent>
+                                <Link to='/form' style={{ textDecoration: 'none' }}>
+                                    <Button 
+                                        onClick={this.sendData}
+                                        variant='contained' 
+                                        size='large' 
+                                        style={{ color: 'white' }} 
+                                        className={classNames(this.props.classes.cssRoot)}
+                                    >
+                                        Apply Now
+                                        <i className="material-icons">
+                                            arrow_forward
+                                        </i>
+                                    </Button>
+                                </Link>
+                            </CardContent>
+                            <CardContent>
+                                It's <b>free</b> & won't affect your credit score
+                            </CardContent>
+                        </Card>
+                    </ Grid>
+                </Grid>
+            </div>
             )
         }
 }
